afdac70d5b2a8fcea159a3a9af9d6d7f5adaf8c2
Quoc Anh <quocanh1897@gmail.com>
2018-09-21 14:43:22 +0700

    init

---
+++ src/sample/Main.java
@@ -0,0 +1,31 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    public static Stage stage = null;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Ui.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
+        this.stage = stage;
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/sample/UiController.java
@@ -0,0 +1,67 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class UiController implements Initializable {
+
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+    @FXML
+    private AnchorPane parent;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        makeStageDrageable();
+    }
+
+    private void makeStageDrageable() {
+        parent.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        parent.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Main.stage.setX(event.getScreenX() - xOffset);
+                Main.stage.setY(event.getScreenY() - yOffset);
+                Main.stage.setOpacity(0.7f);
+            }
+        });
+        parent.setOnDragDone((e) -> {
+            Main.stage.setOpacity(1.0f);
+        });
+        parent.setOnMouseReleased((e) -> {
+            Main.stage.setOpacity(1.0f);
+        });
+
+    }
+
+    @FXML
+    private void close(MouseEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    private void maximize(MouseEvent event) {
+    }
+
+    @FXML
+    private void minimize(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
+
+}
